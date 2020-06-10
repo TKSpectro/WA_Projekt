69fb171fed005f468b70ce9edb9c0abc36173d38
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-06-10 19:18:42 +0200

    add update and delete for tasksController Closes #7

--- controllers/api/tasksController.js
+++ controllers/api/tasksController.js
@@ -122,6 +122,99 @@ class ApiTasksController extends Controller {
             });
         }
     }
+
+    async actionUpdate() {
+        const self = this;
+
+        //task should be a object with all the values (new and old)
+        let remoteData = self.param('task');
+        let taskId = self.param('id');
+
+        let task = null;
+        let error = null;
+
+        //get the old task
+        try {
+            task = await self.db.Task.findOne({
+                where: {
+                    id: taskId
+                },
+                attributes: ['id', 'name', 'text', 'createdAt', 'updatedAt'],
+                include: self.db.Task.extendInclude
+            }).then(task => {
+                if (task) {
+                    //update the task in db
+                    task.update({
+                        name: remoteData['name'],
+                        text: remoteData['text'],
+                        creatorId: remoteData['creatorId'],
+                        assignedToId: remoteData['assignedToId'],
+                        projectId: remoteData['projectId'],
+                        updatedAt: new Date()
+                    });
+
+                    return task;
+                }
+            });
+        } catch (err) {
+            error = err;
+        }
+
+        if (error !== null) {
+            console.error(error);
+            self.render({
+                details: error
+            }, {
+                statusCode: 500
+            });
+        } else {
+            self.render({
+                task: task
+            });
+        }
+    }
+
+    async actionDelete() {
+        const self = this;
+
+        let taskId = self.param('id');
+
+        let task = null;
+        let error = null;
+
+        //get the old task
+        try {
+            task = await self.db.Task.findOne({
+                where: {
+                    id: taskId
+                },
+                attributes: ['id', 'name', 'text', 'createdAt', 'updatedAt'],
+                include: self.db.Task.extendInclude
+            }).then(task => {
+                if (task) {
+                    //update the task in db
+                    task.destroy();
+
+                    return task;
+                }
+            });
+        } catch (err) {
+            error = err;
+        }
+
+        if (error !== null) {
+            console.error(error);
+            self.render({
+                details: error
+            }, {
+                statusCode: 500
+            });
+        } else {
+            self.render({
+                task: task
+            });
+        }
+    }
 }
 
 module.exports = ApiTasksController;
