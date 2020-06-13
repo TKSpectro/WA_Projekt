c9dc4161cb98f80d9560ff8d1973962e069667a2
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-06-13 16:54:37 +0200

    fix database connections

--- config/database.js
+++ config/database.js
@@ -1,25 +1,26 @@
 require('dotenv').config();
 
 module.exports = {
-    development : {
+    development: {
         username: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
         database: process.env.DB_NAME || 'taskboard',
-        host:     process.env.DB_HOST || 'localhost',
-        dialect:  'mysql'
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        debug: true
     },
-    test : {
-        username: 'root',
-        password: '',
-        database: 'taskboard',
-        host:     'localhost',
-        dialect:  'mysql'
+    test: {
+        username: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'taskboard',
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
     },
-    production : {
-        username: 'root',
-        password: '',
-        database: 'taskboard',
-        host:     'localhost',
-        dialect:  'mysql'
+    production: {
+        username: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'taskboard',
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
     }
 }
