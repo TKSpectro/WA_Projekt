5c2cdb7bef96c30b786975feeee995d6ec02d314
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-06-13 17:42:50 +0200

    update our demo users

--- src/db/seeders/20200515155227-demo-user.js
+++ src/db/seeders/20200515155227-demo-user.js
@@ -4,18 +4,18 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('user', [{
             id: 1,
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'doey@mail.com',
+            firstName: 'Tom',
+            lastName: 'Käppler',
+            email: 't@mail.com',
             createdAt: new Date(),
             updatedAt: new Date(),
             passwordHash: '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
             permission: 0b1111111111111111
         }, {
             id: 2,
-            firstName: 'Helga',
-            lastName: 'Travis',
-            email: 'h.travis@mail.com',
+            firstName: 'Bilal',
+            lastName: 'Alnaani',
+            email: 'b@mail.com',
             createdAt: new Date(),
             updatedAt: new Date(),
             passwordHash: '$2b$10$nsNf6L5XPv6wOHcC.oWzCuh/dDeFPuEmSYm5IZ4iJyOLe4OAsrIA2', // bCrypt 10 12345678
