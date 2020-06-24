982439b05e8a26537ea9f5345b9190455db11584
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-06-24 12:22:14 +0200

    add permission defines for apiDoc

---
+++ src/apidoc/api/helper.js
@@ -0,0 +1,45 @@
+/**
+ * @apiDefine canDeleteUser
+ * Permission to delete users without being the owner of the account
+ */
+
+/**
+* @apiDefine canUpdateUser
+* Permission to update users without being the owner of the account
+*/
+
+/**
+* @apiDefine canDeleteTask
+* Permission to delete tasks without being the owner of the task
+*/
+
+/**
+* @apiDefine canUpdateTask
+* Permission to update tasks without being the owner or assignee of the task
+*/
+
+/**
+* @apiDefine canCreateTask
+* Permission to create tasks
+*/
+
+/**
+* @apiDefine canDeleteProject
+* Permission to delete project
+*/
+
+/**
+* @apiDefine canUpdateProject
+* Permission to update project
+*/
+
+/**
+* @apiDefine canCreateProject
+* Permission to create project
+*/
+
+
+/**
+* @apiDefine isUserDeleted
+* Flags a user as deleted
+*/
