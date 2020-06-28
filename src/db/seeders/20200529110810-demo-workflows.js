c058c79d7513cdf106ad404d38a9357444f2a628
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-06-28 19:07:48 +0200

    Update 20200529110810-demo-workflows.js

--- src/db/seeders/20200529110810-demo-workflows.js
+++ src/db/seeders/20200529110810-demo-workflows.js
@@ -4,28 +4,28 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('workflow', [{
             name: 'TODO',
-            color: '#666666',
+            color: '#2ecc71',
             sort: 1,
             projectId: 1,
             createdAt: new Date(),
             updatedAt: new Date(),
         },{
             name: 'In Progress',
-            color: '#666666',
+            color: '#3498db',
             sort: 2,
             projectId: 1,
             createdAt: new Date(),
             updatedAt: new Date(),
         },{
             name: 'Review',
-            color: '#666666',
+            color: '#9b59b6',
             sort: 3,
             projectId: 1,
             createdAt: new Date(),
             updatedAt: new Date(),
         },{
             name: 'Done',
-            color: '#666666',
+            color: '#f1c40f',
             sort: 4,
             projectId: 1,
             createdAt: new Date(),
