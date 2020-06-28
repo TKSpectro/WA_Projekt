e4d00afbe1d50d999732c7ecfb15ab5a6a5857a8
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-06-28 13:15:24 +0200

    add copy to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
  * @version 1.0.0
  */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         // Reference to all Plugun, that we have
         pkg: grunt.file.readJSON('package.json'),
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
                     ],
                     compress: true,
                     plugins: [
-                        new(require('less-plugin-autoprefix'))({
+                        new (require('less-plugin-autoprefix'))({
                             browsers: [
                                 'last 2 versions',
                                 'ie 9'
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
             build: {
                 files: {
                     'assets/js/index.min.js': 'src/js/index.js',
-                    'assets/js/signin.min.js': 'src/js/signin.js'
+                    'assets/js/signin.min.js': 'src/js/signin.js',
                 }
             }
         },
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
                     'src/less/**',
                     'src/js/**'
                 ],
-                tasks: ['less', 'uglify'],
+                tasks: ['less', 'uglify', 'copy'],
             }
         },
         apidoc: {
@@ -56,7 +56,15 @@ module.exports = function(grunt) {
                 src: "src/apidoc/",
                 dest: "docs/"
             }
-        }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    { src: 'node_modules/html5sortable/dist/html5sortable.min.js', dest: 'assets/js/html5sortable.min.js'},
+                ],
+            },
+        },
     });
 
 
@@ -66,9 +74,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-apidoc');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     //register plugins
-    grunt.registerTask('build', ['less', 'uglify', 'apidoc']);
+    grunt.registerTask('build', ['less', 'uglify', 'copy', 'apidoc']);
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('apiDoc', ['apidoc']);
 }
