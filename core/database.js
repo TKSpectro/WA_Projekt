645cbdf2b0815efcec1458c2aea2cddce44d183e
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-07-02 15:35:18 +0200

    Update database.js

--- core/database.js
+++ core/database.js
@@ -16,7 +16,8 @@ module.exports = function () {
             min: 0,
             acquire: 30000,
             idle: 10000
-        }
+        },
+        //logging:false,
     });
 
     const db = {
